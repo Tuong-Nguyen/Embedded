27118853f1e966e8d38e6a4baf41d3bb999fbc47
Thao Le <thaoln87@gmail.com>
2017-07-06 13:44:26 +0700

    Set up Unity test framework

---
+++ src/DumbExample.h
@@ -0,0 +1,11 @@
+//
+// Created by nctuong on 7/6/2017.
+//
+
+#ifndef CTUTORIAL_DUMBEXAMPLE_H
+#define CTUTORIAL_DUMBEXAMPLE_H
+
+#include <stdint.h>
+
+int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c);
+#endif //CTUTORIAL_DUMBEXAMPLE_H
